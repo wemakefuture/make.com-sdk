69644b7a07edffda7006aa6cb37a73a065748a54
Dominik <d.p.arnold@gmx.de>
2022-05-25 22:22:32 +0200

    Corrected Issues in folder tests

--- src/lib/folders.test.ts
+++ src/lib/folders.test.ts
@@ -7,23 +7,26 @@ const make = new Make({
   organizationId: 94920,
 });
 
+let folderId: number;
+
 test('testCreateFolder', async () => {
   const testCreateFolder = await make.createFolder({ name: 'Folder 3', teamId: 60004 });
+  folderId = testCreateFolder.scenarioFolder.id;
   //  console.log(testCreateFolder);
   expect(testCreateFolder['scenarioFolder']).toBeDefined();
 });
 
-test('testDeleteFolder', async () => {
-  const testDeleteFolder = await make.deleteFolder({ folderId: 24696 });
-  expect(testDeleteFolder['scenarioFolder']).toEqual(24696);
-});
-
 test('testUpdateFolder', async () => {
-  const testUpdateFolder = await make.updateFolder({ scenarioFolderId: 24624, name: 'Temp 1' });
+  const testUpdateFolder = await make.updateFolder({ scenarioFolderId: folderId, name: 'Temp 1' });
   expect(testUpdateFolder['scenarioFolder'].name).toEqual('Temp 1');
 });
 
 test('testListFolders', async () => {
   const testListFolders = await make.listFolders({ teamId: 60004 });
   expect(testListFolders['scenariosFolders'].length).toBeGreaterThanOrEqual(0);
+});
+
+test('testDeleteFolder', async () => {
+  const testDeleteFolder = await make.deleteFolder({ folderId: folderId });
+  expect(testDeleteFolder['scenarioFolder']).toEqual(folderId);
 });
